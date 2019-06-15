16a74e8cf03a6f148968e1fa78561e8011ae9382
Briii <brianwkhuffman@gmail.com>
2019-06-14 19:15:17 -1000

    #8 done

--- js/app.js
+++ js/app.js
@@ -38,8 +38,13 @@ newPrice2.innerHTML = '$34.99';
 newItem.appendChild(newPrice2);
 
 //8. Create a button element with the id of 'discount3' and give it a content of 50% OFF Axe body soap!. Append this element to the appropriate div class 'block3.
+let newbutton = document.createElement('button');
+newbutton.id = 'discount3';
+newbutton.innerHTML = '50% OFF Axe body soap!';
+price3.appendChild(newbutton);
 
 //9. Target the div element with a class of 'item' for the Panda Express section and update the content to Orange Chicken.
+
 
 //10. Create a button element with a class of 'discount' and update the content to Free T-shirt and fortune cookie with purchase! Append this button to Panda Express section.
 
